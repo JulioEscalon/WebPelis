ea73278dd0440d96e28800f02d787b98d0e296f4
JulioEscalon <julio.escalon.s@uni.pe>
2020-04-01 00:34:51 -0500

    CineEnCasa 2.0

--- funciones.js
+++ funciones.js
@@ -1,28 +1,16 @@
-function enviar(){
-    var mensaje = document.getElementById('msg-text').value;
-    var iniciales = document.getElementById('usuario');
-
-    //--------Creando nodo
-    var caja= document.createElement('div');
-    var texto= document.createTextNode(mensaje);
-    //----- Poniendo el texto dentro del div
-    caja.appendChild(texto);
-    //--- Poniendo el atributo class al div caja
-    caja.setAttribute('class','globo');
-    
-    //--- Creando el nodo para meter mis dos cosas
-    var nuevo = document.createElement('div');
-    nuevo.setAttribute('class','union');
-    //--- Definiendo las variables para introducir el div nuevo
-    var union=document.getElementById('union');
-
-
-
-
-    //--- Probando :'v
-    /*union.apendTo(iniciales);
-    union.apendTo(caja);
-
-    //---Poniendo el div dentro del otro div :'v
-    msgbox.apendTo(union);*/
+window.onload  = function(){
+    document.getElementById("primero").onclick = seleccionarPrimero;
+    document.getElementById("segundo").onclick = seleccionarSegundo;
+}
+function seleccionarPrimero(){
+    var primero =  document.getElementById("uno");
+    var segundo =  document.getElementById("dos");
+    primero.className="selected";
+    segundo.className="opcion";
+}
+function seleccionarSegundo(){
+    var primero =  document.getElementById("uno");
+    var segundo =  document.getElementById("dos");
+    primero.className="opcion";   
+    segundo.className="selected";
 }
